dcd0417a23b9c9d1167f5b21ea05b0e6850d61ce
Milica Siriski <milica.siriski998@gmail.com>
2021-07-09 16:25:52 +0200

    Added users

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -2,18 +2,18 @@
 INSERT INTO public.auth_role (id, name) VALUES ('1', 'NISTAGRAM_USER_ROLE');
 INSERT INTO public.auth_role (id, name) VALUES ('2', 'ADMINISTRATOR_ROLE');
 
--- INSERT INTO public.nistagram_user(id, enabled, username)
--- 	VALUES ('1', true, '2017-10-01 21:58:58.508-07', 'luka');
--- INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
--- 	VALUES ('2', true, '2017-10-01 21:58:58.508-07', 'vlado');
--- INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
--- 	VALUES ('3', true, '2017-10-01 21:58:58.508-07', 'vidoje');
--- INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
--- 	VALUES ('4', true, '2017-10-01 21:58:58.508-07', 'milica');
--- INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
--- 	VALUES ('5', true, '2017-10-01 21:58:58.508-07', 'duja');
--- INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
--- 	VALUES ('6', true, '2017-10-01 21:58:58.508-07', 'kobra');
+INSERT INTO public.nistagram_user(id, enabled, username)
+	VALUES ('1', true, '2017-10-01 21:58:58.508-07', 'luka');
+INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
+	VALUES ('2', true, '2017-10-01 21:58:58.508-07', 'vlado');
+INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
+	VALUES ('3', true, '2017-10-01 21:58:58.508-07', 'vidoje');
+INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
+	VALUES ('4', true, '2017-10-01 21:58:58.508-07', 'milica');
+INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
+	VALUES ('5', true, '2017-10-01 21:58:58.508-07', 'duja');
+INSERT INTO public.nistagram_user(id, enabled, last_password_reset_date, username)
+	VALUES ('6', true, '2017-10-01 21:58:58.508-07', 'kobra');
 
 
 
