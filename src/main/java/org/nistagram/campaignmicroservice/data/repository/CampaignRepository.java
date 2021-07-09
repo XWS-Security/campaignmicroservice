640ede3708b080d17f608043fa817f672d1a8e5a
Vladimir Budjen <bvalda88@gmail.com>
2021-07-09 13:53:24 +0200

    Adds are shown on influencers page

--- src/main/java/org/nistagram/campaignmicroservice/data/repository/CampaignRepository.java
+++ src/main/java/org/nistagram/campaignmicroservice/data/repository/CampaignRepository.java
@@ -10,6 +10,6 @@ public interface CampaignRepository extends CrudRepository<Campaign, Long> {
     List<Campaign> findAllByAgentAccountUsername(String id);
 
     @Query(value = "SELECT c.campaign_type, c.id, c.agent_account_username, c.deleted, c.gender, c.max_age, c.min_age, c.exposure_end, c.exposure_start, c.last_update, c.required_daily_displays, c.exposure_date, c.advertisement_id" +
-            " FROM hire_request as hr, campaign as c where c.id=hr.campaign_id", nativeQuery = true)
+            " FROM hire_request as hr, campaign as c where c.id=hr.campaign_id and hr.id=:id", nativeQuery = true)
     Campaign findByHireRequestId(Long id);
 }
--- src/main/java/org/nistagram/campaignmicroservice/data/repository/HireRequestRepository.java
+++ src/main/java/org/nistagram/campaignmicroservice/data/repository/HireRequestRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface HireRequestRepository extends CrudRepository<HireRequest, Long> {
-    @Query(value = "SELECT distinct id, approval_status, influencer_username, campaign_id FROM hire_request where influencer_username=:username and approval_status=0",  nativeQuery = true)
+    @Query(value = "SELECT distinct id, approval_status, influencer_username, campaign_id FROM hire_request where influencer_username=:username and approval_status=0", nativeQuery = true)
     List<HireRequest> findAllByInfluencerUsernameAndWaitingStatus(String username);
 
-    @Query(value = "SELECT distinct id, approval_status, influencer_username, campaign_id FROM hire_request where influencer_username=:username and campaign_id=:campaignId",  nativeQuery = true)
+    @Query(value = "SELECT distinct id, approval_status, influencer_username, campaign_id FROM hire_request where influencer_username=:username and campaign_id=:campaignId", nativeQuery = true)
     List<HireRequest> findAllByInfluencersUsernameCampaignId(String username, Long campaignId);
+
+    @Query(value = "SELECT distinct id, approval_status, influencer_username, campaign_id FROM hire_request where influencer_username=:username and campaign_id=:campaignId and approval_status=1", nativeQuery = true)
+    List<HireRequest> findAllByInfluencersUsernameCampaignIdAndApproved(String username, Long campaignId);
 
 }
