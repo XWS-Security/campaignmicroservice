f91cc1ab81c56f5d39f2e95a20fa47b21922d4f8
Milica Siriski <milica.siriski998@gmail.com>
2021-07-09 21:14:59 +0200

    Report data can be fetched

--- src/main/java/org/nistagram/campaignmicroservice/service/impl/CampaignStatsServiceImpl.java
+++ src/main/java/org/nistagram/campaignmicroservice/service/impl/CampaignStatsServiceImpl.java
@@ -8,14 +8,16 @@ import org.nistagram.campaignmicroservice.data.model.User;
 import org.nistagram.campaignmicroservice.data.repository.CampaignRepository;
 import org.nistagram.campaignmicroservice.exceptions.UserDoesNotExistException;
 import org.nistagram.campaignmicroservice.service.CampaignStatsService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CampaignStatsServiceImpl implements CampaignStatsService {
     private final CampaignRepository campaignRepository;
 
-    public CampaignStatsServiceImpl(CampaignRepository campaignRepository, CurrentlyLoggedUserService currentlyLoggedUserService) {
+    public CampaignStatsServiceImpl(CampaignRepository campaignRepository) {
         this.campaignRepository = campaignRepository;
     }
 
@@ -25,6 +27,7 @@ public class CampaignStatsServiceImpl implements CampaignStatsService {
         if (user == null) throw new UserDoesNotExistException();
         List<Campaign> campaigns = campaignRepository.findAllByAgentAccountUsername(user.getUsername());
         return campaigns.stream().map(campaign -> {
+            // TODO: get likes, dislikes and comments
             var result = new GetAgentReportResponse(campaign.getId(), 1, 2, 3);
             if (campaign instanceof ContinuousCampaign) {
                 ContinuousCampaign c = (ContinuousCampaign) campaign;
